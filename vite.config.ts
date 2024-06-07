6a62550160fed11e9d26ad3c0d738b53bfc7a3a0
Bereket Abera <Bereket.Abera@zemenbank.local>
2024-06-07 21:35:28 +0300

    Inital Commit

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    NodeGlobalsPolyfillPlugin({
+      process: true,
+      buffer: true,
+    }),
+    NodeModulesPolyfillPlugin()
+  ],
+  resolve: {
+    alias: {
+      util: 'util/',
+      stream: 'stream-browserify',
+      buffer: 'buffer/',
+      process: 'process/browser',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser global polyfills
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  }
+});
